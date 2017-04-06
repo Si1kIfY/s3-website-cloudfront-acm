249f2011d759e100a4cccdececb203053dbc8ca9
Callum Silcock <github@csi.lk>
2017-04-06 15:12:17 +1000

    successfully can get and set the s3 bucket policy

--- src/index.js
+++ src/index.js
@@ -7,11 +7,51 @@ program
   .command('*')
   .action((domain) => {
     console.log('creating bucket: "%s"', domain)
-    console.log(process.env)
+    // TODO: Parse domain input to make sure it's a URL
     s3.createBucket({
-
+      Bucket: domain,
     }, (err, bucket) => {
+      if (err && err.code !== 'BucketAlreadyOwnedByYou') {
+        throw new Error(err)
+      }
+      console.log(bucket)
+    })
+  })
 
+program
+  .command('set-policy')
+  .action((domain) => {
+    console.log('setting policy')
+    s3.putBucketPolicy({
+      Bucket: domain,
+      Policy: JSON.stringify({
+        Statement: {
+          Sid: 'AddPublicReadPermissions',
+          Effect: 'Allow',
+          Principal: '*',
+          Action: 's3:GetObject',
+          Resource: `arn:aws:s3:::${domain}/*`,
+        },
+      }),
+    }, (err, data) => {
+      if (err) {
+        throw new Error(err)
+      }
+      console.log(data)
+    })
+  })
+
+program
+  .command('get-policy')
+  .action((domain) => {
+    console.log('getting policy')
+    s3.getBucketPolicy({
+      Bucket: domain,
+    }, (err, data) => {
+      if (err && err.code !== 'NoSuchBucketPolicy') {
+        throw new Error(err)
+      }
+      console.log(data)
     })
   })
 
--- src/library/s3.js
+++ src/library/s3.js
@@ -1,14 +1,14 @@
 import AWS from 'aws-sdk'
 
-const mock = new AWS.S3({
-  s3ForcePathStyle: true,
-  endpoint: new AWS.Endpoint('http://localhost:4568'),
-  region: 'ap-southeast-2',
-})
-
-// const s3 = new AWS.S3({
+// const mock = new AWS.S3({
+//   s3ForcePathStyle: true,
+//   endpoint: new AWS.Endpoint('http://localhost:4568'),
 //   region: 'ap-southeast-2',
 // })
 
-export default mock
-// export default s3
+const s3 = new AWS.S3({
+  region: 'ap-southeast-2',
+})
+
+// export default mock
+export default s3
